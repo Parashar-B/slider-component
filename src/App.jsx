a6ef401320f34283c4a43cd33279e199158215d9
Parashar-B <parasharbharali21@gmail.com>
2023-11-21 13:50:53 +0530

    basic slider added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Slider from "./components/Slider"
+
+function App() {
+  
+  return (
+    <div className="bg-blue-100 h-[100vh] flex items-center justify-center">
+      <Slider/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Slider.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import { FcPrevious,FcNext } from "react-icons/fc";
+
+export default function Slider(){
+    const imgList = ['1.webp', '2.webp', '3.webp', '4.jpg', '5.jpg', '6.jpg']
+    const [activeImg, setActiveImg] = useState(0);
+    const [disablePrevButton, setDisablePrevButton] = useState(true);
+    const [disableNextButton, setDisableNextButton] = useState(false);
+    const [zoomPrevIcon, setZoomPrevIcon] = useState(false);
+    const [zoomNextIcon, setZoomNextIcon] = useState(false);
+
+    function renderPrevImg(){
+        setActiveImg(activeImg-1);
+    }
+
+    function renderNextImg(){
+        setActiveImg(activeImg+1)
+    }
+
+    useEffect(()=>{
+        if(activeImg === 0)
+            setDisablePrevButton(true);
+        else if(activeImg === imgList.length-1)
+            setDisableNextButton(true);
+        else {
+            setDisablePrevButton(false);
+            setDisableNextButton(false);
+        }
+    },[activeImg])
+
+    return(
+        <div className="w-[1500px] h-[680px] bg-white px-[2rem] pt-[2rem] drop-shadow-lg">
+            <div className=" h-[85%] outline outline-1 outline-gray-200">
+                <img
+                    src={`/images/${imgList[activeImg]}`}
+                    alt={"img"}
+                    className="w-full h-full"
+                />
+            </div>
+            <div className="h-[15%] ">
+               <div className="h-[30%] flex items-center justify-center">
+                    {
+                        imgList.map((item,index)=>{
+                            return(
+                                <div key={index} className={`w-[10px] h-[10px] rounded-full mx-[0.2rem] ${activeImg === index ? 'bg-gray-500':'bg-gray-300'}`}>
+                                </div>
+                            )
+                        })
+                    }
+               </div>
+               <div className="h-[70%]  flex items-center justify-center">
+                    <div className="w-[150px] h-full flex justify-around items-center ">
+                        <button className="bg-gray-200 w-[50px] h-[72%] rounded-full text-[1.5rem] font-bold flex items-center justify-center cursor-pointer" 
+                            disabled={disablePrevButton}
+                            onClick={renderPrevImg}
+                            onMouseOver={()=>{if(!disablePrevButton)setZoomPrevIcon(true)}}
+                            onMouseLeave={()=>{setZoomPrevIcon(false)}}
+                        >
+                            <FcPrevious size={zoomPrevIcon ? '1.5rem':'1rem'}/>
+                        </button>
+                        <button className="bg-gray-200 w-[50px] h-[72%] rounded-full text-[1.5rem] font-bold flex items-center justify-center"
+                            disabled={disableNextButton}
+                            onClick={renderNextImg}
+                            onMouseOver={()=>{if(!disableNextButton)setZoomNextIcon(true)}}
+                            onMouseLeave={()=>{setZoomNextIcon(false)}}
+                        >
+                            <FcNext size={zoomNextIcon ? '1.5rem':'1rem'}/>
+                        </button>
+                    </div>
+               </div>
+            </div>
+        </div>
+    )
+}
