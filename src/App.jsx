a7599ef30d354648f33728d6d150b5e656e06f5e
Parashar-B <parasharbharali21@gmail.com>
2023-11-21 17:01:08 +0530

    few updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import Slider from "./components/Slider"
 function App() {
   
   return (
-    <div className="bg-blue-100 h-[100vh] flex items-center justify-center">
+    <div className="bg-blue-100 h-[100vh] flex items-center justify-center relative">
       <Slider/>
+      <div className="absolute top-0 left-[0] px-[2.2rem] py-[1rem] w-[180px] h-[170px] bg-green-400 rounded-br-[90%] drop-shadow-md">
+        <h1 className="font-bold text-[2rem]">The <br/> Slider</h1>
+      </div>
     </div>
   )
 }
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { FcPrevious,FcNext } from "react-icons/fc";
+import {MdOutlineArrowBackIos, MdOutlineArrowForwardIos} from 'react-icons/md'
 
 export default function Slider(){
-    const imgList = ['1.webp', '2.webp', '3.webp', '4.jpg', '5.jpg', '6.jpg']
+    const imgList = ['1.jpg', '2.webp', '3.webp', '4.webp', '5.jpg', '6.jpg']
     const [activeImg, setActiveImg] = useState(0);
     const [disablePrevButton, setDisablePrevButton] = useState(true);
     const [disableNextButton, setDisableNextButton] = useState(false);
@@ -30,12 +31,34 @@ export default function Slider(){
 
     return(
         <div className="w-[1500px] h-[680px] bg-white px-[2rem] pt-[2rem] drop-shadow-lg">
-            <div className=" h-[85%] outline outline-1 outline-gray-200">
+            <div className=" h-[85%] outline outline-1 outline-gray-200 relative">
                 <img
                     src={`/images/${imgList[activeImg]}`}
                     alt={"img"}
                     className="w-full h-full"
                 />
+                 <div className="absolute top-[40%]">
+                    <button className="bg-gray-200 w-[90px] h-[100px] rounded-r-full text-[1.5rem] font-bold flex items-center justify-center opacity-50" 
+                        disabled={disablePrevButton}
+                        onClick={renderPrevImg}
+                        onMouseOver={()=>{if(!disablePrevButton)setZoomPrevIcon(true)}}
+                        onMouseLeave={()=>{setZoomPrevIcon(false)}}
+                        title={disablePrevButton? 'disabled' : 'prev'}
+                    >
+                        <MdOutlineArrowBackIos size={zoomPrevIcon ? '3.5rem':'2.5rem'}/>
+                    </button>
+                </div>
+                <div className="absolute top-[40%] right-0">
+                    <button className="bg-gray-200 w-[90px] h-[100px] rounded-l-full text-[1.5rem] font-bold flex items-center justify-center opacity-50"
+                        disabled={disableNextButton}
+                        onClick={renderNextImg}
+                        onMouseOver={()=>{if(!disableNextButton)setZoomNextIcon(true)}}
+                        onMouseLeave={()=>{setZoomNextIcon(false)}}
+                        title={disableNextButton? 'disabled':'next'}
+                    >
+                        <MdOutlineArrowForwardIos size={zoomNextIcon ? '3.5rem':'2.5rem'}/>
+                    </button>
+                </div>
             </div>
             <div className="h-[15%] ">
                <div className="h-[30%] flex items-center justify-center">
@@ -47,14 +70,16 @@ export default function Slider(){
                             )
                         })
                     }
+                   
                </div>
                <div className="h-[70%]  flex items-center justify-center">
                     <div className="w-[150px] h-full flex justify-around items-center ">
-                        <button className="bg-gray-200 w-[50px] h-[72%] rounded-full text-[1.5rem] font-bold flex items-center justify-center cursor-pointer" 
+                        <button className="bg-gray-200 w-[50px] h-[72%] rounded-full text-[1.5rem] font-bold flex items-center justify-center" 
                             disabled={disablePrevButton}
                             onClick={renderPrevImg}
                             onMouseOver={()=>{if(!disablePrevButton)setZoomPrevIcon(true)}}
                             onMouseLeave={()=>{setZoomPrevIcon(false)}}
+                            title={disablePrevButton? 'disabled' : 'prev'}
                         >
                             <FcPrevious size={zoomPrevIcon ? '1.5rem':'1rem'}/>
                         </button>
@@ -63,6 +88,7 @@ export default function Slider(){
                             onClick={renderNextImg}
                             onMouseOver={()=>{if(!disableNextButton)setZoomNextIcon(true)}}
                             onMouseLeave={()=>{setZoomNextIcon(false)}}
+                            title={disableNextButton? 'disabled':'next'}
                         >
                             <FcNext size={zoomNextIcon ? '1.5rem':'1rem'}/>
                         </button>
